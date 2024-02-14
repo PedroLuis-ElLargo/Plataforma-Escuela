18dea30edaed1cb31f15011db1a95255d2b5f3b1
Pedro Luis Valenzuela <mlb99100@gmail.com>
2024-02-14 16:27:24 -0400

    1er arreglo

--- js/index.js
+++ js/index.js
@@ -21,6 +21,18 @@ function Agregar() {
         }
     }
 
+    // Llamar a la función para crear una nueva instancia de Estudiante
+    crearNuevoEstudiante(editNombre, editApellido, editMatricula, editCurso, editCalificacion);
+
+    // Ordenar la base de datos por nombre
+    baseDeDatos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+    actualizarTabla();
+    LimpiarCelda();
+}
+
+// Definir una función para crear una nueva instancia de Estudiante
+function crearNuevoEstudiante(nombre, apellido, matricula, curso, calificacion) {
     function Estudiante(nombre, apellido, matricula, curso, calificacion) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -29,59 +41,103 @@ function Agregar() {
         this.calificacion = calificacion;
     }
 
-    let nuevoEstudiante = new Estudiante(editNombre, editApellido, editMatricula, editCurso, editCalificacion);
+    let nuevoEstudiante = new Estudiante(nombre, apellido, matricula, curso, calificacion);
     baseDeDatos.push(nuevoEstudiante);
-
-    // Ordenar la base de datos por nombre
-    baseDeDatos.sort((a, b) => a.nombre.localeCompare(b.nombre));
-
-    // Actualizar la tabla
-    actualizarTabla();
-
-    LimpiarCelda();
 }
 
+
 function actualizarTabla() {
+
     const tbody = document.getElementById("table-body");
     tbody.innerHTML = '';
     for (let estudiante of baseDeDatos) {
         tbody.innerHTML += `<tr class="padreTabla"> <td>${estudiante.nombre}</td><td>${estudiante.apellido}</td> <td>${estudiante.matricula}</td> <td>${estudiante.curso}</td> <td>${estudiante.calificacion}</td> <td> <input type="submit" class="btn-update btn btn-primary" onClick="EditarCelda(this)" value="Actualizar"> <input type="submit" class="btn-delete btn btn-danger" onclick="EliminarCelda(this)" value="Eliminar"></td></tr>`;
     }
+
 }
 
 function LimpiarCelda() {
+
     document.getElementById("nombre").value = '';
     document.getElementById("apellido").value = '';
     document.getElementById("matricula").value = '';
     document.getElementById("curso").value = '';
     document.getElementById("calificacion").value = '';
+
 }
 
 // Editar un estudiante con una función
 function EditarCelda(editar) {
+
+     const botonAgregar = document.getElementById("agregar");
+     const botonEditar = document.getElementById("editar");
+     botonAgregar.style.display = "none";
+     botonEditar.style.display = "inline";
+
     let fila = editar.parentNode.parentNode;
     const datosEditados = {
-        nombre: fila.cells[0].textContent,
-        apellido: fila.cells[1].textContent,
-        matricula: fila.cells[2].textContent,
-        curso: fila.cells[3].textContent,
-        calificacion: fila.cells[4].textContent,
+        nombre: fila.cells[0].innerHTML,
+        apellido: fila.cells[1].innerHTML,
+        matricula: fila.cells[2].innerHTML,
+        curso: fila.cells[3].innerHTML,
+        calificacion: fila.cells[4].innerHTML,
     }
     document.getElementById("nombre").value = datosEditados.nombre;
     document.getElementById("apellido").value = datosEditados.apellido;
     document.getElementById("matricula").value = datosEditados.matricula;
     document.getElementById("curso").value = datosEditados.curso;
     document.getElementById("calificacion").value = datosEditados.calificacion;
+
+    const editMatricula = document.getElementById("matricula");
+    editMatricula.disabled = true;
+
 }
 
 document.getElementById("form_agregar").addEventListener('submit', function (evento) {
     evento.preventDefault();
-    Agregar();
-})
 
-function EliminarCelda(eventoEliminar) {
-    const celda = eventoEliminar.parentNode.parentNode; // Llamado del td
-    const index = Array.from(celda.parentNode.children).indexOf(celda);
-    baseDeDatos.splice(index, 1); // Eliminar el estudiante de la base de datos
-    celda.remove(); // Eliminar la celda de la tabla
+    const editNombre = document.getElementById("nombre").value;
+    const editApellido = document.getElementById("apellido").value;
+    const editMatricula = document.getElementById("matricula").value;
+    const editCurso = document.getElementById("curso").value;
+    const editCalificacion = document.getElementById("calificacion").value;
+
+    const filaOriginal = document.querySelector( ".table tr.padreTabla" );
+    filaOriginal.innerHTML = `
+        <td>${ editNombre }</td>
+        <td>${ editApellido }</td>
+        <td>${ editMatricula }</td>
+        <td>${ editCurso }</td>
+        <td>${ editCalificacion }</td>
+        <td>
+            <input type="submit" class="btn-update btn btn-primary" onClick="EditarCelda(this)" value="Actualizar">
+            <input type="submit" class="btn-delete btn btn-danger" onclick="EliminarCelda(this)" value="Eliminar">
+        </td>
+    `;
+    filaOriginal.classList.remove( "padreTabla" );
+    document.getElementById( "form_agregar" ).reset();
+    
+    crearNuevoEstudiante(editNombre, editApellido, editMatricula, editCurso, editCalificacion);
+    let index = baseDeDatos.findIndex(estudiante => estudiante.matricula === editMatricula);
+    if (index !== -1) {
+        baseDeDatos.splice(index, 1);
+    }
+    console.log( {baseDeDatos} );
+
+    const botonAgregar = document.getElementById( "agregar" );
+    const botonEditar = document.getElementById( "editar" );
+    botonAgregar.style.display = "inline";
+    botonEditar.style.display = "none";
+    setTimeout(function () {
+        document.getElementById("matricula").disabled = false;
+    }, 0);
+});
+
+function EliminarCelda( eventoEliminar ) {
+
+    const celda = eventoEliminar.parentNode.parentNode;
+    const index = Array.from( celda.parentNode.children ).indexOf( celda );
+    baseDeDatos.splice(  index, 1 );
+    celda.remove();
+
 }
